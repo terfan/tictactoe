92b8318f77071bd546c03059afeeda6ebab0b422
Teresa Fan <teresaefan@gmail.com>
2013-01-19 11:41:15 -0500

    drawGrid

---
+++ src/drawGrid.java
@@ -0,0 +1,99 @@
+
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.GridLayout;
+
+public class drawGrid extends Frame {
+    public static void main(String argv[]) {
+        drawGrid fa = new drawGrid();
+        fa.setLayout(new GridLayout(9, 9));
+        fa.setSize(400, 400);
+        fa.setVisible(true);
+    }
+
+    drawGrid() {
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+        add(new Button(" "));
+
+    }// End of constructor
+}// End of Application
+
