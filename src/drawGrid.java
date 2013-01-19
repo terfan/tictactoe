d37834a313531f5e70ccb61df6fa52af69aa4c76
Teresa Fan <teresaefan@gmail.com>
2013-01-19 12:27:57 -0500

    drawPanel

--- src/drawGrid.java
+++ src/drawGrid.java
@@ -1,99 +1,37 @@
-
-import java.awt.Button;
-import java.awt.Frame;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class drawGrid extends Frame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class drawGrid extends JFrame implements ActionListener {
     public static void main(String argv[]) {
         drawGrid fa = new drawGrid();
-        fa.setLayout(new GridLayout(9, 9));
+        fa.setLayout(new GridLayout(3, 3));
         fa.setSize(400, 400);
         fa.setVisible(true);
     }
 
     drawGrid() {
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
-        add(new Button(" "));
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                drawPanel();
+            }
+                for (int i = 0; i < 81; i++) {
+                    for (int i = 0; i < 81; i++) {
+
+            JButton button = new JButton();
+            button.addActionListener(this);
+            }
+            }
+            }
+        }
 
     }// End of constructor
+
+    public void actionPerformed(ActionEvent ae) {
+        jtf.setText(ae.getActionCommand());
+    }
 }// End of Application
 
---
+++ src/drawPanel.java
@@ -0,0 +1,13 @@
+
+/*public class drawPanel extends JPanel implements ActionListener {
+ for (int i = 0; i < 9; i++) {
+ JButton button = new JButton();
+ button.addActionListener(this);
+ }
+ public static void main(String[] args) {
+ }
+
+ public void actionPerformed(ActionEvent ae) {
+ jtf.setText(ae.getActionCommand());
+ }
+ }*/
