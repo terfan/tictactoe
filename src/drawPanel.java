1d202af8ee973bfda18b2979b68ba2a2ef5b44ff
Rebecca <Rebecca@seas1242.wireless-pennnet.upenn.edu>
2013-01-19 12:35:13 -0500

    panel

--- src/drawPanel.java
+++ src/drawPanel.java
@@ -1,13 +1,20 @@
 
-/*public class drawPanel extends JPanel implements ActionListener {
- for (int i = 0; i < 9; i++) {
- JButton button = new JButton();
- button.addActionListener(this);
- }
- public static void main(String[] args) {
- }
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import javax.swing.JPanel;
+
+public class drawPanel extends JPanel implements ActionListener {
+	public drawPanel(){
+		 for (int i = 0; i < 9; i++) {
+			 JButton button = new JButton();
+			 button.addActionListener(this);
+			 }
+	}
 
  public void actionPerformed(ActionEvent ae) {
- jtf.setText(ae.getActionCommand());
+ ae.getActionCommand();
  }
- }*/
+ }
