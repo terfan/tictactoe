c005d92a8684f773a3cab7571959d1a727945c40
Rebecca <Rebecca@seas1242.wireless-pennnet.upenn.edu>
2013-01-19 10:52:33 -0500

    finished winning

--- src/BigGrid.java
+++ src/BigGrid.java
@@ -8,4 +8,45 @@ public class BigGrid {
             }
         }
     }
+    
+    public boolean isWon() {
+        return checkHoriz0() || checkVert0() || checkDiag01() || checkDiag02();
+    }
+
+    public boolean checkHoriz0() {
+        
+        if (grids[0][0].isWon() == true && grids[0][0].isWon() == grids[0][1].isWon() && grids[0][1].isWon() == grids[0][2].isWon()
+        		|| grids[1][0].isWon() == true && grids[1][0].isWon() == grids[1][1].isWon() && grids[1][1].isWon() == grids[1][2].isWon()
+        		|| grids[2][0].isWon() == true && grids[2][0].isWon() == grids[2][1].isWon() && grids[2][1].isWon() == grids[2][2].isWon()) {
+             return true;             
+        }
+        return false;
+    }
+
+    public boolean checkVert0() {
+        if (grids[0][0].isWon() == true && grids[0][0].isWon() == grids[1][0].isWon() && grids[1][0].isWon() == grids[2][0].isWon()
+        		|| grids[0][1].isWon() == true && grids[0][1].isWon() == grids[1][1].isWon() && grids[1][1].isWon() == grids[2][1].isWon()
+        		|| grids[0][2].isWon() == true && grids[0][2].isWon() == grids[1][2].isWon() && grids[1][2].isWon() == grids[2][2].isWon()) {
+             return true;      
+        }
+        
+        return false;
+    }
+
+    public boolean checkDiag01() {
+        if (grids[0][0].isWon() == true  && grids[0][0].isWon() == grids[1][1].isWon() && grids[1][1].isWon() == grids[2][2].isWon()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkDiag02() {
+        if (grids[0][2].isWon() == true && grids[0][2].isWon() == grids[1][1].isWon() && grids[1][1].isWon() == grids[2][0].isWon()) {
+            return true;
+        }
+        return false;
+    }
 }
+
+    
+
